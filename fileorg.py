0b0c67e356d9b86cc8736e8ea740701ba0a5fddb
Dark-Devloper_2024 <hamzamdali111@gmail.com>
2026-01-13 22:35:57 +0530

    Fixed Some Errors Here and there

--- fileorg.py
+++ fileorg.py
@@ -18,12 +18,27 @@ class FileScanner:
             dir_info[file] = (path, "folder" if os.path.isdir(path) else "file")
         return dir_info
 
+    def get_cat(self, file):
+        category = {
+            "Images": (".png", ".jpg", ".jpeg", ".webp", ".bmp", ".gif"),
+            "Documents": (".pdf", ".docx", ".xlsx", ".txt", ".csv", ".pptx"),
+            "Videos": (".mp4", ".mov", ".avi", ".mkv", ".wmv"),
+            "Audios": (".wav", ".mp3", ".aac"),
+            "Programs": (".py", ".java", ".cs", ".js", ".php", ".sql", ".html"),
+            "Exe": (".exe"),
+        }
+        for cat, extension in category.items():
+            if file.lower().endswith(extension):
+                return cat
+        return "Misc"
+
     def filter_files(self, category):
         filtered = {}
         files = self.scan_directory()
         if not files:
             return {}
         if category == "type":
+
             filtered = {
                 "Images": [],
                 "Documents": [],
@@ -37,28 +52,8 @@ class FileScanner:
             for file in files:
                 path = files[file][0]
                 if files[file][1] == "file":
-                    if file.lower().endswith(
-                        (".png", ".jpg", ".jpeg", ".webp", ".bmp", ".gif")
-                    ):
-                        filtered["Images"].append(path)
-                    elif file.lower().endswith(
-                        (".pdf", ".docx", ".xlsx", ".txt", ".csv", ".pptx")
-                    ):
-                        filtered["Documents"].append(path)
-                    elif file.lower().endswith(
-                        (".mp4", ".mov", ".avi", ".mkv", ".wmv")
-                    ):
-                        filtered["Videos"].append(path)
-                    elif file.lower().endswith((".wav", ".mp3", ".aac")):
-                        filtered["Audios"].append(path)
-                    elif file.lower().endswith(
-                        (".py", ".java", ".cs", ".js", ".php", ".sql", ".html")
-                    ):
-                        filtered["Programs"].append(path)
-                    elif file.lower().endswith(".exe"):
-                        filtered["Exe"].append(path)
-                    else:
-                        filtered["Misc"].append(path)
+                    catg = self.get_cat(file)
+                    filtered[catg].append(path)
 
                 else:
                     filtered["Folders"].append(path)
@@ -96,54 +91,46 @@ class FileMover:
     def __init__(self, filtered_data, base):
         self.data = filtered_data
         self.base = base
-        self.dry_run = False
         self.moves = []
 
     def mover(self, path, folder):
         try:
             shutil.move(path, os.path.join(self.base, folder))
         except FileExistsError:
-            print("Destination path already exists")
+            print(f"Destination path {folder} already exists")
 
     def folder_creator(self):
         for cat, path in self.data.items():
             if not path:
                 continue
-            if not os.path.exists(cat):
-                os.makedirs(os.path.join(self.base, cat), exist_ok=True)
+            folder_path = os.path.join(self.base, cat)
+            if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
+
+    def dry_runner(self):
+        for cat, paths in self.data.items():
+            if not paths:
+                continue
+            for path in paths:
+                print(f"Moving {os.path.basename(path)} to {cat} Folder")
 
     def file_mover(self):
-
-        ch = input("Dry Run Before Organizing?(y/n)")
-        if ch.lower() == "y":
-            self.dry_run = True
-        if self.dry_run == True:
-            for cat, paths in self.data.items():
-                if not paths:
-                    continue
-                for path in paths:
-                    print(f"Moving {os.path.basename(path)} to {cat}...")
-            c = input("Continue OR Exit?(c/e)")
-            if c.lower() == "c":
-                self.dry_run = False
-        if self.dry_run == False:
-            self.folder_creator()
-            for cat, paths in self.data.items():
-                if not paths:
-                    continue
-                for path in paths:
-                    self.mover(path, cat)
-                    self.moves.append((path, os.path.join(self.base, cat)))
+        self.folder_creator()
+        for cat, paths in self.data.items():
+            if not paths:
+                continue
+            for path in paths:
+                self.mover(path, cat)
+                self.moves.append((path, os.path.join(self.base, cat)))
 
     def Undo(self):
         for paths in self.moves:
-            files = os.listdir(paths[1])
-            for file in files:
-                path = os.path.join(paths[1], file)
-                self.mover(path, self.base)
+            # change the self.moves to give the base name directly instead of changing hre
+            file_path = os.path.join(paths[1], os.path.basename(paths[0]))
+            self.mover(file_path, self.base)
         for paths in self.moves:
             if os.path.exists(paths[1]):
-                shutil.rmtree(paths[1])
+                shutil.rmtree(paths[1], ignore_errors=True)
 
 
 class MainEngine:
@@ -151,12 +138,20 @@ class MainEngine:
         scanner = FileScanner(path)
         data = scanner.filter_files(typ)
         mover = FileMover(data, path)
-        mover.file_mover()
-        ch = input("Undo? OR Continue?(u/c)")
-        if ch.lower() == "u":
-            mover.Undo()
+        dry_run = input("Dry Run Before Moving(y/n): ")
+        if dry_run.lower() == "y":
+            mover.dry_runner()
+            ch = input("Continue (y/n): ").lower()
+            if ch != "y":
+                return
+            mover.file_mover()
         else:
-            return
+            mover.file_mover()
+            ch = input("Undo? OR Continue?(u/c)")
+            if ch.lower() == "u":
+                mover.Undo()
+            else:
+                return
 
     def main(self):
         print("-" * 32)
@@ -165,7 +160,7 @@ class MainEngine:
         while True:
             path = input("Enter The Path of the folder or disk to organize: ")
             if os.path.exists(rf"{path}"):
-                ch = int(input("1. Sort By Type\n2. Sort By Size\n3. Sort By Time"))
+                ch = int(input("1. Sort By Type\n2. Sort By Size\n3. Sort By Time: "))
                 if ch == 1:
                     self.worker("type", path)
                 elif ch == 2:
